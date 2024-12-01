a3f0cce2c6c2469d878d1223d5881f4017fe8851
swapnilk5 <157964617+swapnilk5@users.noreply.github.com>
2024-12-01 20:57:31 +0530

    Delete StudentDBMS (Frontend)/common.js

--- StudentDBMS (Frontend)/common.js
+++
@@ -1,8 +0,0 @@
-function setActiveNavLink(activeElement) {
-    // Remove "active" class from all nav links
-    const navLinks = document.querySelectorAll('.nav-link');
-    navLinks.forEach((link) => link.classList.remove('active'));
-
-    // Add "active" class to the clicked link
-    activeElement.classList.add('active');
-  }
